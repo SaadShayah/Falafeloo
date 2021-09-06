8056e7fbd9c3af9a35db9d0f31484d2108102fa4
saad <saad.shayah@gmail.com>
2021-09-06 14:02:23 +0300

    some responsiveness

--- JS/main.js
+++ JS/main.js
@@ -1,19 +1,9 @@
+// require("../node_modules/vue");
 // import Vue from "../node_modules/vue";
-// import Carousel3d from "../node_modules/vue-carousel-3d";
+// import Carousel3d from "./vue-carousel-3d";
+// import { Carousel3d, Slide } from "../node_modules/vue-carousel-3d";
+// require("../vue-carousel-3d");
 
-// Vue.use(Carousel3d);
-
-// Vue.createApp({
-//   data: {
-//     slides: 7,
-//   },
-//   components: {
-//     Carousel,
-//     Slide,
-//     "carousel-3d": window["carousel-3d"].Carousel3d,
-//     slide: window["carousel-3d"].Slide,
-//   },
-// }).mount("#example");const menu = document.getElementById("menu");
 const home = document.getElementById("home");
 const contact = document.getElementById("contact");
 const about = document.getElementById("about");
@@ -22,14 +12,31 @@ const rectangle = document.querySelector(".rectangle");
 const sideBtn = document.querySelector(".side-btn");
 const ourSideBar = document.getElementById("sideBar");
 
+// Vue.use(Carousel3d);
+
+// new Vue({
+//   el: "#example",
+//   data: {
+//     slides: 7,
+//   },
+//   components: {
+//     "carousel-3d": window["carousel-3d"].Carousel3d,
+//     slide: window["carousel-3d"].Slide,
+//   },
+// });
+
+//---------------------------- THE SIDE NAVIGATION --------------------------//
+
 menu.addEventListener("click", function (event) {
   container.style.transform = "translateY(-4.5rem)";
   rectangle.style.height = "72vh";
 });
+
 home.addEventListener("click", function name(params) {
   container.style.transform = "translateY(0)";
   rectangle.style.height = "85vh";
 });
+
 contact.addEventListener("click", function name(params) {
   container.style.transform = "translateY(-9rem)";
   rectangle.style.height = "63vh";
@@ -41,48 +48,32 @@ about.addEventListener("click", function name(params) {
 });
 
 sideBtn.addEventListener("click", function name(params) {
+  ourSideBar.classList.remove("hide");
   ourSideBar.classList.add("show");
-  console.log("clicked");
 });
 
-document.addEventListener("click", (evt) => {
-  let targetElement = evt.target; // clicked element
+//---------------------------- HANDELING CLICKS OUTSIDE OF THE SIDEBAR --------------------------//
 
-  do {
-    if (targetElement == ourSideBar) {
-      // This is a click inside. Do nothing, just return.
-      return;
-    }
-    // Go up the DOM
-    targetElement = targetElement.parentNode;
-  } while (targetElement);
+var ignoreClickOnMeElement = document.getElementById("sideBar");
 
-  // This is a click outside.
-  ourSideBar.classList.remove("show");
-  ourSideBar.classList.add("hide");
+document.addEventListener("click", function (event) {
+  var isClickInsideElement = ignoreClickOnMeElement.contains(event.target);
+  if (
+    !isClickInsideElement &&
+    !ignoreClickOnMeElement.classList.contains("hide")
+  ) {
+    ourSideBar.classList.remove("show");
+    ourSideBar.classList.add("hide");
+  }
 });
+
+//---------------------------- THE BURGER NAVIGATION --------------------------//
 
 let burger = document.getElementById("burger"),
-  nav = document.getElementById("main-nav"),
-  slowmo = document.getElementById("slowmo");
+  nav = document.getElementById("main-nav");
 
 burger.addEventListener("click", function (e) {
   this.classList.toggle("is-open");
   nav.classList.toggle("is-open");
-});
-
-/* Onload demo - dirty timeout */
-let clickEvent = new Event("click");
-
-window.addEventListener("load", function (e) {
-  slowmo.dispatchEvent(clickEvent);
-  burger.dispatchEvent(clickEvent);
-
-  setTimeout(function () {
-    burger.dispatchEvent(clickEvent);
-
-    setTimeout(function () {
-      slowmo.dispatchEvent(clickEvent);
-    }, 3500);
-  }, 5500);
+  document.body.classList.toggle("lock");
 });
